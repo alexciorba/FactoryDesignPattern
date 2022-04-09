376460b21f5bc78da96514ba21cf44df680ca834
Ciorba Alex <alexciorba28@gmail.com>
2022-04-09 15:32:48 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NotificationFactory notificationFactory = new NotificationFactory();
+        Notification notification = notificationFactory.createNotification("SMS");
+        notification.notifyUser();
+    }
+}
